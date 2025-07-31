3a717b6bad263e739626a85ccd0ca1d768576f50
kntt32 <153593861+kntt32@users.noreply.github.com>
2025-07-31 19:36:54 +0900

    bugfix

--- test/main.c
+++ test/main.c
@@ -1,28 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-static int numbers[100000];
-static int numbers_len = 0;
-
 int main() {
-    for(int i=2; i<100000; i++) {
-        bool prime_flag = true;
-        for(int k=0; i<numbers_len; k++) {
-            if(i % numbers[k] == 0) {
-                prime_flag = false;
-                break;
-            }
-        }
-        if(prime_flag) {
-            numbers[numbers_len] = i;
-            numbers_len ++;
-        }
+    long long sum = 0;
+    for(long long i=0; i<1000000000; i++) {
+        sum += i;
     }
-
-    for(int i=0; i<numbers_len; i++) {
-        printf("%d, ", numbers[i]);
-    }
-    printf("\n");
+    printf("%lld\n", sum);
 
     return 0;
 }

0b86a9cf626a102c78023cfd1200c24297799c6d
knt.t <153593861+kntt32@users.noreply.github.com>
2025-08-11 09:46:57 +0000

    bugfix on install script 2

--- install/install.bash
+++ install/install.bash
@@ -10,8 +10,11 @@ cp ../stdlibs/std.amc "$HOME/.asmacro/std.amc"
 cp ../stdlibs/core "$HOME/.asmacro/core" -r
 cp ../stdlibs/core.amc "$HOME/.asmacro/core.amc"
 cp ../stdlibs/buildstdlibs.bash "$HOME/.asmacro/buildstdlibs.bash"
+echo >> "$HOME/.bashrc"
 echo 'export ASMACRO_STDLIBS="$HOME/.asmacro/"' >> "$HOME/.bashrc"
 echo 'export PATH="$HOME/.asmacro/bin:$PATH"' >> "$HOME/.bashrc"
-source ~/.bashrc
+echo >> "$HOME/.bashrc"
+export ASMACRO_STDLIBS="$HOME/.asmacro/"
+export PATH="$HOME/.asmacro/bin:$PATH"
 source "$HOME/.asmacro/buildstdlibs.bash"
 
--- stdlibs/buildstdlibs.bash
+++ stdlibs/buildstdlibs.bash
@@ -1,9 +1,9 @@
 #!/usr/bin/bash
 
 cd "$(dirname "${BASH_SOURCE[0]}")"
-./bin/asmacro std.amc
-./bin/asmacro core.amc
-./bin/asmacro std/parser.amc
+asmacro std.amc
+asmacro core.amc
+asmacro std/parser.amc
 
 ar rcs std.a std.o std/parser.o
 ar rcs core.a core.o
